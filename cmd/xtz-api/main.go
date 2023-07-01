3882c2c3c4ee35177cbb7b84056dcc664e44b73c
Duc Van Trang <trang.ducvan@icloud.com>
2023-07-01 21:01:15 +0200

    feat: add cmd/xtz-api app

---
+++ cmd/xtz-api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ducvan-hpy/xtz-api/cmd/xtz-api/api"
+	"github.com/ducvan-hpy/xtz-api/cmd/xtz-api/poller"
+	"github.com/ducvan-hpy/xtz-api/internal/domain/repository"
+	"github.com/ducvan-hpy/xtz-api/internal/infrastructure/persistence"
+)
+
+const (
+	host         = "localhost"
+	port         = 8090
+	pollInterval = 10 * time.Second
+)
+
+func main() {
+	repo := &repository.Repository{
+		Delegation: persistence.NewInMemoryStorage(),
+	}
+
+	restAPI := api.New(repo)
+	engine := api.NewGinRouter(restAPI)
+
+	// Run poller in goroutine.
+	delegationPoller := poller.New("tzktSDK", pollInterval, repo)
+	go delegationPoller.Start(context.Background())
+
+	serverURL := fmt.Sprintf("%s:%d", host, port)
+	err := http.ListenAndServe(serverURL, engine)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
